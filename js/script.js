0f808a40cd85203decb5ab1e64fdfdf93a71de2c
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-05-20 17:21:41 +0200

    chats contacts

--- js/script.js
+++ js/script.js
@@ -10,3 +10,6 @@ var app = new Vue(
 
 } 
 );
+
+// Replica della grafica (immagine in allegato) con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse;
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto, ricavandoli dall'array contacts qui allegato
